e23a5d81233e518e9c907a30264e5a24b806ec27
甲元 祥朗 <yoshi-komoto@i-mobilegroup.com>
2026-07-05 17:44:34 +0900

    ちぇあー1秒

--- webapp/go/chair_handlers.go
+++ webapp/go/chair_handlers.go
@@ -201,7 +201,7 @@ func chairGetNotification(w http.ResponseWriter, r *http.Request) {
 	if err := tx.GetContext(ctx, ride, `SELECT * FROM rides WHERE chair_id = ? ORDER BY updated_at DESC LIMIT 1`, chair.ID); err != nil {
 		if errors.Is(err, sql.ErrNoRows) {
 			writeJSON(w, http.StatusOK, &chairGetNotificationResponse{
-				RetryAfterMs: 30,
+				RetryAfterMs: 1000,
 			})
 			return
 		}
@@ -261,7 +261,7 @@ func chairGetNotification(w http.ResponseWriter, r *http.Request) {
 			},
 			Status: status,
 		},
-		RetryAfterMs: 30,
+		RetryAfterMs: 1000,
 	})
 }
 
