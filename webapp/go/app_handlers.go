9ace982cdf9682797e44f231caa388313642840e
= <=>
2026-07-20 06:29:58 +0000

    Revert "１秒"
    This reverts commit cecff8bd88771fb94f0cd8eb9919dfdd1d386718.

--- webapp/go/app_handlers.go
+++ webapp/go/app_handlers.go
@@ -673,7 +673,7 @@ func appGetNotification(w http.ResponseWriter, r *http.Request) {
 	if err := tx.GetContext(ctx, ride, `SELECT * FROM rides WHERE user_id = ? ORDER BY created_at DESC LIMIT 1`, user.ID); err != nil {
 		if errors.Is(err, sql.ErrNoRows) {
 			writeJSON(w, http.StatusOK, &appGetNotificationResponse{
-				RetryAfterMs: 1000,
+				RetryAfterMs: 30,
 			})
 			return
 		}
@@ -720,7 +720,7 @@ func appGetNotification(w http.ResponseWriter, r *http.Request) {
 			CreatedAt: ride.CreatedAt.UnixMilli(),
 			UpdateAt:  ride.UpdatedAt.UnixMilli(),
 		},
-		RetryAfterMs: 1000,
+		RetryAfterMs: 30,
 	}
 
 	if ride.ChairID.Valid {
